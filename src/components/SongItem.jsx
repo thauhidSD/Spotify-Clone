fad920db29d33bb909cac4f9effeebd35a427e75
Md Thauhid Hossain <thethauhid103@gmail.com>
2026-05-02 19:38:25 +0600

    Create music album page

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const SongItem = ({ name, image, desc, id }) => {
+
   return (
-    <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+    <div onClick={()=>navigate(`//${id}`)}className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
         <img className='rounded' src={image} alt="" />
         <p className='font-bold mt-2 mb-1'>{name}</p>
         <p className='text-slate-200 text-sm'>{desc}</p> {/* Also changed 'desc' to {desc} */}
