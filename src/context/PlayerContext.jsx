e2914947ae5deaba56730f61746b3f4bda41061a
Md Thauhid Hossain <thethauhid103@gmail.com>
2026-05-04 20:15:17 +0600

    Display current time and song duration

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useRef, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import { songsData } from "../assets/assets";
 
 export const PlayerContext = createContext();
@@ -13,11 +13,11 @@ const PlayerContextProvider = (props) => {
     const [time,setTime] = useState({
         currentTime:{
             second:0,
-            minnute:0
+            minute:0
         },
         totalTime:{
             second:0,
-            minnute:0
+            minute:0
         }
     })
 
@@ -31,6 +31,24 @@ const PlayerContextProvider = (props) => {
         setPlayStatus(false);
 
     }
+
+    useEffect(() => {
+        setTimeout(() => {
+
+            audioRef.current.ontimeupdate = () => {
+                setTime({
+                    currentTime:{
+                        second: Math.floor(audioRef.current.currentTime % 60),
+                        minute: Math.floor(audioRef.current.currentTime / 60)
+                    },
+                    totalTime:{
+                        second: Math.floor(audioRef.current.duration % 60),
+                        minute: Math.floor(audioRef.current.duration / 60)
+                    }
+                })
+            }
+        }, 1000);
+    },[audioRef])
 
     const contextValue = {
         audioRef,
